8de444fc1df17ac29238e5e10113a9a0607e250d
aijishh <AIjishu2789@nycstudents.net>
2024-09-25 09:20:23 -0400

    sept 25 commit

---
+++ bill.py
@@ -0,0 +1,9 @@
+service = input('bad? okay? good? great?')
+if service == "bad":
+    print('5%')
+elif service == "okay":
+    print('10%')
+elif service == "good":
+    print('15%')
+elif service == "great":
+    print('20%')
--- oddoreven.py
+++ oddoreven.py
@@ -1,5 +1,5 @@
 odd_or_even = int(input("Odd? Or Even?"))
 if odd_or_even % 2 == 0:
-    print("The number is even.")
+    print('The number inputted is an even integer.')
 else:
-    print("The number is odd.")
+    print('The number inputted is an odd integer.')
