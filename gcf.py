c831d8878be780da86d9529df7433f25063bfe9f
aijishh <AIjishu2789@nycstudents.net>
2024-09-27 09:17:02 -0400

    sept 27 commit

---
+++ gcf.py
@@ -0,0 +1,8 @@
+num1 = int(input('number'))
+num2 = int(input('number'))
+gcf = 1
+
+for i in range(1, max(num1, num2)):
+    if num1 % i == 0 and num2 % i == 0:
+        gcf = i
+print (f'{gcf} is the gcf')
